1dfae296618d3209bf646cbdc239b3af76b64519
Kamila Sabori <a22311054@uthermosillo.edu.mx>
2024-01-26 20:12:57 -0700

    proyecto android

---
+++ primerapi/app/src/main/java/com/uth/kamila/primerapi/ModuleFragment.kt
@@ -0,0 +1,20 @@
+package com.uth.kamila.primerapi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ModuleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_module, container, false)
+        return view
+    }
+}
